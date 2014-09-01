95c0c86324f21e1ec4b29909b05c95155a7b0676
Ramon Octavio Badillo Rivas <robr8@hotmail.com>
2014-09-01 15:19:02 -0500

    mini mate test

---
+++ game.py
@@ -0,0 +1,10 @@
+class game:
+    scoreP1=0
+    scoreP2=0
+    
+    def __init__(self, nameP1, nameP2):
+        self.nameP1 = nameP1
+        self.nameP2 = nameP2
+        
+    def score(self,):
+    print "score"
