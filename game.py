bdc152af1b763d1a5d13be2d1a3712b42dfd8ad9
Ramon Octavio Badillo Rivas <robr8@hotmail.com>
2014-09-02 18:19:00 -0500

    ultimo que masomenos jala

--- game.py
+++ game.py
@@ -1,87 +1,116 @@
+'''
+Al inicio del juego cómo debe de estar el score?
+>>> Game().score()
+[0, 0]
+
+Cuando un jugador anota
+>>> Game().scores(1).score()
+[15, 0]
+
+Cuando un jugador anota 3 veces seguidas
+>>> Game().scores(1).scores(1).scores(1).score()
+[40, 0]
+
+Cuando un jugador anota 4 veces seguidas
+>>> Game().scores(1).scores(1).scores(1).scores(1).score()
+[Wins, Loses]
+
+Cuando el otro jugador le empata
+>>> Game().scores(1).scores(1).scores(1).scores(2).scores(2).scores(2).score()
+[Deuce, Deuce]
+
+Cuando estan empatados y uno obtiene la ventaja
+>>> Game().scores(1).scores(1).scores(1).scores(2).scores(2).scores(2).scores(1).score()
+[Adv, ]
+
+Cuando después de tener la ventaja vuelven a empatar
+>>> Game().scores(1).scores(1).scores(1).scores(2).scores(2).scores(2).scores(1).scores(2).score()
+[Deuce', Deuce]
+
+Cuando obtiene la ventaja y además anota otro punto
+>>> Game().scores(1).scores(1).scores(1).scores(2).scores(2).scores(2).scores(1).scores(1).score()
+[Wins, Loses]
+'''
+
 import math
 
-class game:
-    description = ['Love','15','30','40','Adv']
-    scoreP1 = 0
-    scoreP2 = 0
-    nameP1 = ""
-    nameP2 = ""
-    ganador = ""
-    diff = 0
-    cuatros = False
-    scores = ["",""]
+class Game:
     
     
+    description = ['0','15','30','40','Adv']
+    pScores = [0,0]
+    pScoresF = ["",""]
+    flag = False
+    win = False
+    diff =  0
+    empates = False
     
-    def __init__(self, nameP1, nameP2):
-        self.nameP1 = nameP1
-        self.nameP2 = nameP2
-        #print("%s , %s" % (self.description[self.scoreP1], self.description[self.scoreP2]))
-    
-    def scores(self,quien):
-        if self.ganador == "":
-            if quien == self.nameP1:
-                self.scoreP1 += 1
-            elif quien == self.nameP2:
-                self.scoreP2 += 1
-            
-            #self.getWinner()
-            #self.showResults()
+    def __init__(self):pass
+        
+        
+    def scores(self, player):
+        if not self.win:
+            player -=1
+            self.pScores[player] += 1
         else:
-            print("Set Terminado Gano:%s" % (self.ganador))
+            print("Se acabo el set")
+    
+           
+           
+           
+            
+    def score(self):
+        self.diff = math.fabs(self.pScores[0] - self.pScores[1])
+        if self.pScores[0] >= 3 and self.pScores[1] >= 3:
+            self.flag = True
+        elif self.pScores[0] > 3 or self.pScores[1] > 3 and not self.flag:
+            self.getWinner()
+            self.flag = True
+        if self.flag:
+            if self.diff > 1:
+                self.getWinner()
+            elif self.diff == 0:
+                self.pScoresF[0] = 'Deuce'
+                self.pScoresF[1] = 'Deuce'
+            else:
+                self.getAdv()
+        self.showResults()
+        
         return self
         
-    def score(self):
-        self.getWinner()
-        self.showResults()
-    
-    
-    def getWinner(self):
-        
-        if self.scoreP1 >=3 and self.scoreP2 >=3:
-			self.cuatros = True
-			self.diff = math.fabs(self.scoreP1 - self.scoreP2)		
-            
-			if self.diff==0:
-				print "Douce"
-				
-			if self.diff==1:
-				print "Adv"
-				
-			if self.diff > 1:
-				self.getAdvantage()
-				
-		else :
-			if self.scoreP1 >3 or self.scoreP2 >3:
-				self.getAdvantage()
-          
-        
-            
+       
+        if self.diff > 1 and self.cuatros:
+            self.getAdvantage()
         
         
     def showResults(self):
-        print self.cuatros
-		if self.ganador != "":
-            print "Win"
-        elif not self.cuatros:
-            print("%s,%s" % (self.description[self.scoreP1], self.description[self.scoreP2]))
-        
+        if not self.flag:
+            print("[%s, %s]" % (self.description[self.pScores[0]], self.description[self.pScores[1]]))
+        else:
+            print("[%s, %s]" % (self.pScoresF[0], self.pScoresF[1]))
     
-    def getAdvantage(self):
-        if self.scoreP1 > self.scoreP2:
-            self.ganador = self.nameP1
-        elif self.scoreP1 < self.scoreP2:
-            self.ganador = self.nameP2
-           
+    def getAdv(self):
+        if self.pScores[0] > self.pScores[1]:
+            self.pScoresF[0] = 'Adv'
+            self.pScoresF[1] = ''
+        if self.pScores[0] < self.pScores[1]:
+            self.pScoresF[1] = ''
+            self.pScoresF[0] = 'Adv'
             
-game(1,2).scores(1).scores(1).scores(1).scores(2).scores(2).scores(2).scores(1).scores(2).scores(2).score()
-''''
-set1.score(2)
-set1.score(2)
-set1.score(1)
-set1.score(1)
-set1.score(1)
-set1.score(2)
-set1.score(2)
+    def getWinner(self):
+        if self.pScores[0] > self.pScores[1]:
+            self.pScoresF[0] = 'Wins'
+            self.pScoresF[1] = 'Loses'
+        if self.pScores[0] < self.pScores[1]:
+            self.pScoresF[1] = 'Wins'
+            self.pScoresF[0] = 'Loses'
+        self.win = True
+        
 
-'''
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
+    
