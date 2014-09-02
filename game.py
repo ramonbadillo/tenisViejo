57a5c3d39efb0122b308272c6adba4049d4666cf
Ramon Octavio Badillo Rivas <robr8@hotmail.com>
2014-09-02 14:42:26 -0500

    en mi casa

--- game.py
+++ game.py
@@ -1,7 +1,7 @@
 import math
 
 class game:
-    description = ['Love','15','30','40','game']
+    description = ['Love','15','30','40','Adv']
     scoreP1 = 0
     scoreP2 = 0
     nameP1 = ""
@@ -9,6 +9,8 @@ class game:
     ganador = ""
     diff = 0
     cuatros = False
+    scores = ["",""]
+    
     
     
     def __init__(self, nameP1, nameP2):
@@ -23,7 +25,8 @@ class game:
             elif quien == self.nameP2:
                 self.scoreP2 += 1
             
-            
+            #self.getWinner()
+            #self.showResults()
         else:
             print("Set Terminado Gano:%s" % (self.ganador))
         return self
@@ -36,7 +39,6 @@ class game:
     def getWinner(self):
         self.diff = math.fabs(self.scoreP1 - self.scoreP2)
         
-        
         if self.scoreP1 >=3 and self.scoreP2 >=3:
             self.cuatros = True
         
@@ -45,25 +47,33 @@ class game:
             self.getAdvantage()
             
         if self.diff > 1 and self.cuatros:
-            
             self.getAdvantage()
+         
+        '''   
+        if self.cuatros and self.diff==0:
+            print "Douce"
+            
+        if self.cuatros and self.diff==1:
+            print "Adv"
+        '''
         
     def showResults(self):
+        print self.cuatros
         if not self.cuatros:
             print("%s,%s" % (self.description[self.scoreP1], self.description[self.scoreP2]))
         else:
             print("%s,%s" % (self.scoreP1, self.scoreP2))
         if self.ganador != "":
-            print("Felicidades gano: %s" % (self.ganador))   
+            print("Felicidades gano: %s" % (self.ganador))
     
     def getAdvantage(self):
         if self.scoreP1 > self.scoreP2:
             self.ganador = self.nameP1
         elif self.scoreP1 < self.scoreP2:
             self.ganador = self.nameP2
+           
             
-            
-game(1,2).scores(1).scores(2).scores(2).scores(2).score()
+game(1,2).scores(1).scores(1).scores(1).scores(2).scores(2).scores(2).scores(1).scores(2).scores(2).score()
 ''''
 set1.score(2)
 set1.score(2)
